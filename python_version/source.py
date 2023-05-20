159e052c22b582d53ad6b6899912686347d9d4ff
Russell Kajouri <rasoulkajouri@gmail.com>
2023-05-20 20:06:46 +0200

    changing t to T, avoiding confusion

--- python_version/source.py
+++ python_version/source.py
@@ -6,13 +6,13 @@
 #
 from medium import Medium
 def main():
-	dt = 0.100
-	for t in range(1,2):
-		print("temp: {:6.4f}".format(t*dt))
+	dT = 0.100
+	for T in range(1,2):
+		print("temp: {:6.4f}".format(T*dT))
 		medium = Medium(num = 512, dim = 1, initial_config = "stochastic",\
-			initial_direction = 1, J = 1, steps = 70000, temp = t*dt)
+			initial_direction = 1, J = 1, steps = 70000, temp = T*dT)
 		aveE , aveP = medium.Evolution(display=False)
-		print("%-5.3f %-5.3f %-5.3f" %(t*dt,aveE, aveP))
+		print("%-5.3f %-5.3f %-5.3f" %(T*dT,aveE, aveP))
 		medium.WriteTheLattice()
 		print("#", "-"*70)
 		pass
